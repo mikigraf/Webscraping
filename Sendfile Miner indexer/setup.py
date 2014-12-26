d758081a666271846ffb9fd2654d5cd7ec258b3b
Mikolaj Wawrzyniak <mikolajspejs@gmail.com>
2014-12-26 23:26:39 +0100

    Added Sendfile Miner
    script to iterate trough url's at sendfile and scrap the information
    about the links and names of the files hosted

---
+++ Sendfile Miner indexer/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='Sendfile.pl Search Tool',
+    version='0.1',
+    packages=[''],
+    url='',
+    license='',
+    author='FLUTTERSHYSUCKS',
+    author_email='',
+    description=''
+)

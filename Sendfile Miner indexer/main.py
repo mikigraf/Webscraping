6302bd5d39e449efc1c5ab6d1329e6b6d98f9c85
Mikołaj Mike Wawrzyniak <spejss@users.noreply.github.com>
2015-01-03 00:04:16 +0100

    minimized code

--- Sendfile Miner indexer/main.py
+++ Sendfile Miner indexer/main.py
@@ -6,15 +6,10 @@ link = "http://www.sendfile.pl/"
 number = raw_input("PODAJ 6-CIO CYFROWY NUMER PLIKU, OD KTOREGO MA ZACZYNAC SIE LISTA (np. 235000)")
 interwal = raw_input("CO ILE SEKUND MA BYC SPRAWDZANY KOLEJNY PLIK/LINK? ZALECAM CZAS OKOLO 5-15 SEKUND.: ")
 
-
 for number in range(int(number),300000,1):
     time.sleep(int(interwal))
-
-    str(number)
     link2 = "http://www.sendfile.pl/" + str(number)
-    r = requests.get(link2)
-    data = r.text
-    soup = BeautifulSoup(data)
+    soup = BeautifulSoup(requests.get(link2).text)
     for link in soup.find_all('title'):
         title1 = str(soup.find_all('title'))
         print(soup.title)

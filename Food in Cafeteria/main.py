4c35cef735571835c3273164eacdf8303b6a2c1e
Mikolaj Wawrzyniak <mikolajspejs@gmail.com>
2015-01-02 20:12:18 +0100

    refactored code in FiC
    made it more universal, applicable

--- Food in Cafeteria/main.py
+++ Food in Cafeteria/main.py
@@ -1,47 +1,37 @@
 __author__ = 'spejs'
-import mechanize
-from bs4 import BeautifulSoup
-import requests
-import mechanize
-from email.mime.text import MIMEText
 import smtplib
-import time
-from datetime import datetime, timedelta, date
-from threading import Timer
-import re
+from datetime import datetime
 import urllib2
+
+from bs4 import BeautifulSoup
+
 
 mensa = urllib2.urlopen('http://www.stwdo.de/gastronomie/speiseplaene/hauptmensa/').read()
 soup = BeautifulSoup(mensa)
 
-def checkIfInMensa(food,email,passwd):
-    soup2 =  soup.body.get_text();
-    if(food in soup2):
-            print('HAMBURGERS IN MENSA!')
-            print('Email was sent to imspacedreamer@gmail.com')
-            fromaddr = 'pythonmegatron@gmail.com'
-            toaddr = 'imspacedreamer@gmail.com'
-            passwd =
-            msg = "\r\n".join([
-          "From: pythonmegatron@gmail.com",
-          "To: " + email,
-          "Subject: HAMBURGERS IN MENSA",
-          "SPACE IS CHILLIN, MIKE IS CHILLIN, WHAT MORE CAN I SAY? WE KILLIN IT!",
-          ""
-          ])
-            username = 'pythonmegatron@gmail.com'
 
-            server = smtplib.SMTP('smtp.gmail.com:587')
-            server.ehlo()
-            server.starttls()
-            server.login(username, passwd)
-            server.sendmail(fromaddr, toaddr, msg)
-            server.quit()
+def checkIfInMensa(food, fromaddr, toaddr, username, passwd):
+    soup2 = soup.body.get_text();
+    if (food in soup2):
+        print('HAMBURGERS IN MENSA!')
+        print('Email was sent to imspacedreamer@gmail.com')
+    msg = "\r\n".join([
+        "From: " + fromaddr,
+        "To: " + toaddr,
+        "Subject: " + food + " IN MENSA"
+        "SPACE IS CHILLIN, MIKE IS CHILLIN, WHAT MORE CAN I SAY? WE KILLIN IT!",
+        ""
+    ])
+    server = smtplib.SMTP('smtp.gmail.com:587')
+    server.ehlo()
+    server.starttls()
+    server.login(username, passwd)
+    server.sendmail(fromaddr, toaddr, msg)
+    server.quit()
 
     else:
-        currentTime = datetime.now().strftime('%H:%M')
         print(datetime.now().strftime('%H:%M'))
 
-
-checkIfInMensa(food='Hamburger',email='imspacedreamer@gmail.com',passwd=' ')
+checkIfInMensa(food='Hamburger', fromaddr='pythonmegatron@gmail.com', toaddr='imspacedreamer@gmail.com',
+               email='imspacedreamer@gmail.com', username='pythonmegatron@gmail.com', passwd='sadasd ')
 

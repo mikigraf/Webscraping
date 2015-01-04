a48d2c3b7d7ddfe471bc35040ae6f92e8cea55ab
Mikolaj Wawrzyniak <mikolajspejs@gmail.com>
2015-01-04 23:12:14 +0100

    init

--- FB4 News DUOASAA/news_crawler.py
+++ FB4 News DUOASAA/news_crawler.py
@@ -1,10 +1,14 @@
 import smtplib
-import urlib2
+import urllib2
 from bs4 import BeautifulSoup
 
-news = urlib2.urlopen('http://www.fh-dortmund.de/de/fb/4/aktuelles.php').read()
-soup = BeautifulSoup(news)
+
 
 def fetchNews():
+    news = urllib2.urlopen('http://www.fh-dortmund.de/de/fb/4/aktuelles.php').read()
+    soup = BeautifulSoup(news)
     soup2 = soup.body.get_text();
-    
+    print(soup2)
+
+
+fetchNews()

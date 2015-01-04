fade7fc83dc3cac33711c5c59317704a121b6332
Mikolaj Wawrzyniak <mikolajspejs@gmail.com>
2015-01-04 23:35:26 +0100

    broken tho

--- FB4 News DUOASAA/news_crawler.py
+++ FB4 News DUOASAA/news_crawler.py
@@ -5,10 +5,23 @@ from bs4 import BeautifulSoup
 
 
 def fetchNews():
-    news = urllib2.urlopen('http://www.fh-dortmund.de/de/fb/4/aktuelles.php').read()
+    news = urllib2.urlopen('http://www.fh-dortmund.de/de/fb/4/isc/aktuelles/index.php?archive=1').read()
     soup = BeautifulSoup(news)
-    soup2 = soup.body.get_text();
-    print(soup2)
+    file = open('LOG.txt','w')
+    file.write(soup)
+    file.close()
+    print('fetched')
 
+
+def compare():
+    file = open('LOG.txt', 'r')
+    oldNews = file.get_text();
+    newNews = urllib2.urlopen('http://www.fh-dortmund.de/de/fb/4/isc/aktuelles/index.php?archive=1').read()
+    soup = BeautifulSoup(newNews)
+    if(oldNews == newNews ):
+        fetchNews()
+    else:
+        return
 
 fetchNews()
+compare()
